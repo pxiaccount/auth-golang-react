ac75752b8ced719b12225e50a07f3b26f1a40d45
Ken <122612391+pxiaccount@users.noreply.github.com>
2025-06-03 17:54:32 +0700

    CORS and register frontend

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,9 @@ import (
 	"auth-golang-react/controllers"
 	"auth-golang-react/initializers"
 	"auth-golang-react/middleware"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +18,16 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	// Add CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
