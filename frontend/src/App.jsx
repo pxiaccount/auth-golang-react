2bd9d8912873b88d53c71cbd0d200d30bc919e0c
Ken <122612391+pxiaccount@users.noreply.github.com>
2025-06-03 17:33:27 +0700

    Config something

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import "./App.css"
+
 function App() {
   return (
     <>
-
+      <button className="bg-blue-50">A</button>
+      <form action="" method="post" className="border-solid border-2 border-black">
+        <h1>Register</h1>
+        <input type="text" id="Name" />
+        <input type="password" id="Password" />
+      </form>
     </>
   )
 }
